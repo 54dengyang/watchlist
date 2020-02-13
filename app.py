6c874b939ac0f1dd8486133808a3506360dca97e
dengyang <568757336@qq.com>
2020-02-14 02:24:49 +0800

    Add minimal home page

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask
+from flask import url_for
+
+app = Flask(__name__)
+
+@app.route('/')
+@app.route('/index')
+@app.route('/home')
+def hello():
+	# return 'Welcome to My Watchlist!'
+	# return	u'欢迎来到我的Watchlist!'
+	return	'<h1>Hello Totoro!</h1><img src="http://helloflask.com/totoro.gif">'
+
+@app.route('/user/<name>')
+def user_page(name):
+	# return 'User page'
+	return 'User: %s' % name
+
+@app.route('/test')
+def test_url_for():
+	print(url_for('hello'))
+	print(url_for('user_page',name='deng'))
+	print(url_for('user_page',name='yang'))
+	print(url_for('test_url_for'))
+	print(url_for('test_url_for',num=2))
+	return 'Test page'
